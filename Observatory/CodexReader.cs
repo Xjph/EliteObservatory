8c70f46447b71dff2c48f4d4d337918e07f8da06
Xjph <archseraphim@gmail.com>
2020-01-18 16:06:58 -0330

    IGAU transmission changes

--- Observatory/CodexReader.cs
+++ Observatory/CodexReader.cs
@@ -50,10 +50,10 @@ namespace Observatory
             }
 
             RequestQueue.Add(request);
-            SendRequests();
+            var SendTask = SendRequests();
         }
 
-        private static async void SendRequests()
+        private static async Task SendRequests()
         {
             if (!RequestsSending)
             {
@@ -63,15 +63,28 @@ namespace Observatory
                 while (RequestQueue.Count > 0)
                 {
 #if DEBUG
-                    var response =
+                    var response = HttpClient.SendRequestAsync(RequestQueue.First());
+                    System.IO.File.AppendAllText(@"C:\temp\CodexTransmit.log", $"Time: {Environment.TickCount - startTicks}; ResponseCode: {response.Result.StatusCode.ToString()}; Response: {response.Result.Content.ReadAsStringAsync().Result};\r\n");
+                    await Task.Delay(750);
+#else
+                    var SendRequestTask = HttpClient.SendRequestAsync(RequestQueue.First());
+
+                    //Requests complete much more quickly than this, but the IGAU endpoint will start throwing HTTP429 "Too Many Requests"
+                    //so we have to wait a moment between transmissions.
+                    await Task.Delay(1000);
+                    await SendRequestTask;
+
+                    if (!SendRequestTask.Result.IsSuccessStatusCode)
+                    {
+                        System.Windows.Forms.MessageBox.Show($"HTTP Error: {SendRequestTask.Result.StatusCode.ToString()}\r\nContent: {SendRequestTask.Result.Content}\r\nTransmission disabled.", "Error Sending IGAU Data");
+                        RequestQueue.Clear();
+                        mainForm.SetIGAUText(string.Empty);
+                        Properties.Observatory.Default.SendToIGAU = false;
+                        Properties.Observatory.Default.Save();
+                        break;
+                    }
 #endif
 
-#pragma warning disable 4014
-                    HttpClient.SendRequestAsync(RequestQueue.First());
-#if DEBUG
-                    System.IO.File.AppendAllText(@"C:\temp\CodexTransmit.log", $"Time: {Environment.TickCount - startTicks}; ResponseCode: {response.Result.StatusCode.ToString()}; Response: {response.Result.Content.ReadAsStringAsync().Result};\r\n");
-#endif
-                    await Task.Delay(750);
                     RequestQueue.Remove(RequestQueue.First());
 
                     mainForm.SetIGAUText(RequestQueue.Count > 0 ? $"{RequestQueue.Count} pending IGAU transmission{(RequestQueue.Count > 1 ? "s" : "")}.":"");

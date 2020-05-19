80433f9314ece5e7c09ee8c7a701b986e7d15121
Xjph <archseraphim@gmail.com>
2020-05-19 13:14:24 -0230

    Update version number.

--- Observatory/Properties/AssemblyInfo.cs
+++ Observatory/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.20.106")]
-[assembly: AssemblyFileVersion("0.5.20.106")]
+[assembly: AssemblyVersion("0.5.20.140")]
+[assembly: AssemblyFileVersion("0.5.20.140")]
